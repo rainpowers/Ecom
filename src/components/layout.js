323b4011e6333a48016f2e8df8d09aa947448bfb
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-07-17 20:55:12 -0700

    built out header and navbar

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from './headernavbar/header'
+import Navbar from './headernavbar/navbar'
 export default class Layout extends Component {
   render() {
     return (
-      <div>
+      <div className = 'layout'>
         {this.props.children}
+        <Header/>
+        <Navbar/>
       </div>
     );
   }
