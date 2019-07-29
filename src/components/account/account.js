58edf0c5e2051b1b00cf93947957b298750301aa
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-07-28 22:30:48 -0700

    finished header feature

--- src/components/account/account.js
+++ src/components/account/account.js
@@ -10,6 +10,20 @@ class Account extends Component {
 
     componentDidMount() {
 
+        const headerLinks = [
+            {
+                _id: 0,
+                title: 'Shop',
+                path: '/shop'
+            },
+            {
+                _id: 1,
+                title: 'Logout',
+                path: '/'
+            }
+        ]
+
+
         const navbarLinks = [
             {
                 _id: 0,
@@ -25,7 +39,7 @@ class Account extends Component {
             }
         ]
 
-        this.props.setHeaderLinks([]);
+        this.props.setHeaderLinks(headerLinks);
         this.props.setNavbarLinks(navbarLinks);
     }
 
