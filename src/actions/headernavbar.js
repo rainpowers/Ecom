8843731ff473aa9be5f43397b36f29c97872eeb9
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-07-28 22:24:26 -0700

    finished navbar feature

--- src/actions/headernavbar.js
+++ src/actions/headernavbar.js
@@ -1,6 +1,7 @@
 import {
     SET_HEADER_LINKS,
-    SET_NAVBAR_LINKS
+    SET_NAVBAR_LINKS,
+    CHANGE_NAVBAR_ACTIVE
 } from './types';
 
 export function setHeaderLinks(links) {
@@ -15,4 +16,11 @@ export function setNavbarLinks(links) {
         type: SET_NAVBAR_LINKS,
         payload: links
     })
-} 
+}
+
+export function changeNavbarActive(_id) {
+    return ({
+        type: CHANGE_NAVBAR_ACTIVE,
+        payload: _id
+    })
+}
