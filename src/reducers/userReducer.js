0a69089f8b13b825549ab876955c95e187d81b04
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-08-06 13:55:28 -0700

    built account information from from signinform as a template

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -27,8 +27,8 @@ export default function(state = INITIAL_STATE, action) {
             }
             case SET_PURCHASE_DETAIL:
                 let purchaseDetail;
-                state.purchases.map(purchases => {
-                    if(purchase._id = action.payload) {
+                state.purchases.map(purchase => {
+                    if(purchase._id == action.payload) {
                         purchaseDetail = purchase;
                     }
                   
