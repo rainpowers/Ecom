f9578a37dd2534e8b4b924302203f89be326116e
rainpowers <crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.comgit config --global user.email crystalbluerain@gmail.com>
2019-08-12 20:16:57 -0700

    built shop search bar form and form field

--- src/components/shop/shop.js
+++ src/components/shop/shop.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import * as actions from '../../actions';
+import ShopSearchBar from './shopSearchBar';
 
 class Shop extends Component {
 
@@ -27,27 +28,32 @@ class Shop extends Component {
         return true
     }
 
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
+
     render() {
 
         return (
             <div className='shop'>
-                {/* shop search bar */}
-                <div className='shop__products'>
-                    {
-                        this.props.filteredProducts.map(product => {
-                            return (
-                                <div key={product._id} className='shop-product'>
-                                    <div className='shop-product__title'>
-                                        {product.title}
+                <ShopSearchBar onSubmit={this.onSubmit} className='shop__search-bar'/>
+                    <div className='shop__products'>
+                        {
+                            this.props.filteredProducts.map(product => {
+                                return (
+                                    <div key={product._id} className='shop-product'>
+                                        <div className='shop-product__title'>
+                                            {product.title}
+                                        </div>
+                                        <div className='shop-product__description'>
+                                            {product.description}
+                                        </div>
                                     </div>
-                                    <div className='shop-product__description'>
-                                        {product.description}
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
-                </div>
+                                )
+                            })
+                        }
+                    </div>
                 {/* shop cart button */}
             </div>
         )
